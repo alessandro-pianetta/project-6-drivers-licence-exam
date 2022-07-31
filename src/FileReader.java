5943f350380c1f8262e92f09bf5a4d58dfa61bd5
Alessandro Pianetta <alessandro.pianetta@gmail.com>
2022-07-30 21:48:10 -0700

    Formats and asks questions

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,7 +1,33 @@
 import javax.swing.*;
 import java.io.File;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 
 public class FileReader {
+
+    public void readFile (File source, Consumer<String> callback) throws Exception {
+        Scanner input = new Scanner(source);
+        while (input.hasNext()) {
+            String nextLine = input.nextLine();
+            callback.accept(nextLine);
+        }
+        input.close();
+    }
+
+    public void askQuestion(String line) {
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+        String[] question = line.split("\\|", 5);
+        for (String segment:question) {
+            String questionSegment = segment;
+            if (count > 0) {
+                questionSegment = "    " + questionSegment;
+            }
+            count++;
+            System.out.println(questionSegment);
+        }
+    }
     public File chooseFile() {
         JFileChooser fileChooser = new JFileChooser("C:/Users/aapiane09/IdeaProjects/DriversLicenceExam/src");
         File file = null;
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.io.File;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         try {
             FileReader reader = new FileReader();
             File questions = reader.chooseFile();
-            System.out.println(questions);
+            reader.readFile(questions, reader::askQuestion);
         } catch (Exception err) {
             System.out.println("Exception: " + err.getMessage());
         }
