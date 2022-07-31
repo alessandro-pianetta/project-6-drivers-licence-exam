4562b7c41453b707c665cfecb40ad8da70f92334
Alessandro Pianetta <alessandro.pianetta@gmail.com>
2022-07-31 15:58:19 -0700

    Fixes bug where both answer arrays point to same reference

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 import java.io.FileNotFoundException;
 
+/*
+ * @author Alessandro Pianetta
+ * */
 public class Main {
     public static void main(String[] args) {
+        // Instantiates instance of TestGiver for a test with 20 questions
         TestGiver proctor = new TestGiver(20);
         try {
+            // Gives user instructions of how to take the test
             System.out.println("Please enter only 'A', 'B', 'C', or 'D' to answer each question. Case does not matter. Enter 'quit' to exit application.");
+            // Reads question file and asks the questions to the user
             proctor.readFile("assets/questions.txt", proctor::askQuestion);
+            // Reads the answer file and scores the test
             proctor.readFile("assets/DMVanswers.txt", proctor::cacheAndCheckAnswers);
+            // Generates results document
             proctor.generateResults();
         } catch (FileNotFoundException err) {
             System.out.println("Error: File not found! Please confirm that you entered the correct file URI and try again.");
--- src/TestGiver.java
+++ src/TestGiver.java
@@ -7,94 +7,126 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 import java.util.function.BiConsumer;
 
-
+/*
+* @author Alessandro Pianetta
+* */
 public class TestGiver {
+    // Scanner is set as instance variable as I didn't want to create multiple instances while gathering user input for test answers
     private final Scanner input = new Scanner(System.in);
+
+    // Array of Strings that represent stored student answers
     private final String[] studentAnswers;
+
+    // Array of strings to cache test answers so that the file didn't need to be scanned twice
     private final String[] cachedAnswers;
+
+    // Score instance variable as I check points while caching the answers
     private int score = 0;
 
+    // Constructor; accepts an integer representing the # of questions and answers to initialize studentAnswers and cachedAnswers instance variables
     TestGiver (int testLength) {
-        this.studentAnswers = this.cachedAnswers = new String[testLength];
+        this.studentAnswers = new String[testLength];
+        this.cachedAnswers = new String[testLength];
     }
 
+    // Reads and iterates through a file
     public void readFile (String sourceUri, BiConsumer<String, Integer> callback) throws FileNotFoundException {
+        // Gets URI from passed-in String and creates a new File instance
         File sourceFile = new File(sourceUri);
+        // Instantiates Scanner to read file
         Scanner reader = new Scanner(sourceFile);
+        // Counter for keeping track of position in arrays
         int currentLine = 0;
         while (reader.hasNext()) {
+            // For each line of text in the file...
             String nextLine = reader.nextLine();
+            // Run callback method on that line, passing in the currentLine #
             callback.accept(nextLine, currentLine);
+            // Augment current line by 1
             currentLine++;
         }
         reader.close();
     }
 
+    // Asks test taker the questions found in the questions file and gathers answers
     public void askQuestion(String line, int currentLine) throws ArrayIndexOutOfBoundsException {
-            int count = 0;
-            String[] question = line.split("\\|", 5);
-            for (String segment:question) {
-                String questionSegment = segment;
-                if (count > 0) {
-                    questionSegment = "    " + questionSegment;
-                }
-                count++;
-                System.out.println(questionSegment);
+        // Counter for formatting purposes
+        int count = 0;
+        // \n in .txt file was printing '\n' to the console rather than a newline, so I opted for a different formatting method.
+        // Splits question into 5 array items, 1 for the question and 4 for the answers.
+        String[] question = line.split("\\|", 5);
+        for (String segment:question) {
+            String questionSegment = segment;
+            if (count > 0) {
+                // If not the first item, adds indentation for legibility
+                questionSegment = "    " + questionSegment;
             }
+            count++;
+            System.out.println(questionSegment);
+        }
 
-            String answer = this.input.next();
+        // Gathers user answer
+        String answer = this.input.next();
+        // If user enters invalid answer, loops until the user puts in a valid one. Will also exit out when uses inputs 'quit' to console
+        while (!(answer.equalsIgnoreCase("A") || answer.equalsIgnoreCase("B") || answer.equalsIgnoreCase("C") || answer.equalsIgnoreCase("D"))) {
+            System.out.println("Sorry, your answer must be A, B, C, or D. Please try again.");
+            answer = this.input.next();
 
-            while (!(answer.equalsIgnoreCase("A") || answer.equalsIgnoreCase("B") || answer.equalsIgnoreCase("C") || answer.equalsIgnoreCase("D"))) {
-                System.out.println("Sorry, your answer must be A, B, C, or D. Please try again.");
-                answer = this.input.next();
-
-                if (answer.equalsIgnoreCase("quit")) {
-                    System.out.println("Goodbye!");
-                    System.exit(0);
-                }
+            if (answer.equalsIgnoreCase("quit")) {
+                System.out.println("Goodbye!");
+                System.exit(0);
             }
-
-            this.studentAnswers[currentLine] = answer.toUpperCase();
+        }
+        // Adds student answer to array
+        this.studentAnswers[currentLine] = answer.toUpperCase();
     }
 
+    // Adds answers read by readFile into the cachedAnswers array, checks studentAnswers, and tallies up correct answers
     public void cacheAndCheckAnswers (String answer, int currentLine) throws ArrayIndexOutOfBoundsException {
-        cachedAnswers[currentLine] = answer;
+        this.cachedAnswers[currentLine] = answer;
         if (answer.equals(this.studentAnswers[currentLine])) {
-            score++;
+            this.score++;
         }
     }
 
+    // Generates results file
     public void generateResults() throws FileNotFoundException {
         try {
+            // Selects and creates new file within the /results folder
             File results = new File("results/report.txt");
             boolean createdNewFile = results.createNewFile();
 
+            // If the file already exists, will throw exception
             if (!createdNewFile) {
                 throw new FileAlreadyExistsException("File already exists.");
             }
 
+            // If file does not already exist, writes to the file the user's answer, the correct answer, and whether the user was correct or not
             PrintWriter output = new PrintWriter(results);
             int i = 0;
-            for (String studentAnswer:studentAnswers) {
+            for (String studentAnswer:this.studentAnswers) {
                 String yourAnswer = "Your answer was: " + studentAnswer;
-                String cachedAnswer = "The correct answer is: " + cachedAnswers[i];
-                boolean answerIsCorrect = studentAnswer.equals(cachedAnswers[i]);
+                String cachedAnswer = "The correct answer is: " + this.cachedAnswers[i];
+                boolean answerIsCorrect = studentAnswer.equals(this.cachedAnswers[i]);
                 output.println(yourAnswer + " | " + cachedAnswer + " || " + (answerIsCorrect ? "Correct!" : "Incorrect..."));
                 i++;
             }
             output.println("\n" + "**************************************************************" + "\n");
 
-
+            // Calculates percentage of answers correct
             double userPoints = this.score;
             double totalPoints = this.studentAnswers.length;
             double percentageCorrect = userPoints / totalPoints;
-            output.println("Total score: " + (this.score)+ "/20 (" + new DecimalFormat("##%").format(percentageCorrect) + ")\n");
+
+            // Prints the total score in integer and percentage formats
+            output.println("Total score: " + this.score + "/20 (" + new DecimalFormat("##%").format(percentageCorrect) + ")\n");
+
+            // Prints overall result of test. If user got 14 or fewer questions correctly, finalResult is updated to recognize that.
             String finalResult = "Congratulations! You passed with flying colors!";
             if (this.score < 15) {
                 finalResult = "Sorry, you didn't pass. Try again soon!";
             }
             output.println(finalResult);
-
             output.close();
         } catch (FileAlreadyExistsException err) {
             System.out.println("Error: File already exists! Please move, rename, or delete existing file.");
