c7ea4260f535efcd58f83a7b750a157aef6d8e90
Alessandro Pianetta <alessandro.pianetta@gmail.com>
2022-07-31 14:45:13 -0700

    Relegates error handling with 2+ methods that could throw the same error to main function, and one-off exceptions to the method itself.

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.Scanner;
 import java.util.function.BiConsumer;
 
@@ -10,83 +13,90 @@ public class FileReader {
     private final String[] cachedAnswers = new String[20];
     private int score = 0;
 
-    public void readFile (File source, BiConsumer<String, Integer> callback) throws Exception {
-        Scanner reader = new Scanner(source);
+    public void readFile (String sourceUri, BiConsumer<String, Integer> callback) throws FileNotFoundException {
+        File sourceFile = new File(sourceUri);
+        Scanner reader = new Scanner(sourceFile);
         int currentLine = 0;
         while (reader.hasNext()) {
             String nextLine = reader.nextLine();
             callback.accept(nextLine, currentLine);
             currentLine++;
         }
-        input.close();
+        reader.close();
     }
 
-    public void askQuestion(String line, int currentLine) {
-        int count = 0;
-        String[] question = line.split("\\|", 5);
-        for (String segment:question) {
-            String questionSegment = segment;
-            if (count > 0) {
-                questionSegment = "    " + questionSegment;
+    public void askQuestion(String line, int currentLine) throws ArrayIndexOutOfBoundsException {
+            int count = 0;
+            String[] question = line.split("\\|", 5);
+            for (String segment:question) {
+                String questionSegment = segment;
+                if (count > 0) {
+                    questionSegment = "    " + questionSegment;
+                }
+                count++;
+                System.out.println(questionSegment);
             }
-            count++;
-            System.out.println(questionSegment);
-        }
 
-        String answer = this.input.next();
+            String answer = this.input.next();
 
-        while (!(answer.equalsIgnoreCase("A") || answer.equalsIgnoreCase("B") || answer.equalsIgnoreCase("C") || answer.equalsIgnoreCase("D"))) {
-            System.out.println("Sorry, your answer must be A, B, C, or D. Please try again.");
-            answer = this.input.next();
+            while (!(answer.equalsIgnoreCase("A") || answer.equalsIgnoreCase("B") || answer.equalsIgnoreCase("C") || answer.equalsIgnoreCase("D"))) {
+                System.out.println("Sorry, your answer must be A, B, C, or D. Please try again.");
+                answer = this.input.next();
 
-            if (answer.equalsIgnoreCase("quit")) {
-                System.out.println("Goodbye!");
-                System.exit(0);
+                if (answer.equalsIgnoreCase("quit")) {
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                }
             }
-        }
 
-        this.studentAnswers[currentLine] = answer.toUpperCase();
+            this.studentAnswers[currentLine] = answer.toUpperCase();
     }
 
-    public void cacheAndCheckAnswers (String answer, int currentLine) {
+    public void cacheAndCheckAnswers (String answer, int currentLine) throws ArrayIndexOutOfBoundsException {
         cachedAnswers[currentLine] = answer;
         if (answer.equals(this.studentAnswers[currentLine])) {
             score++;
         }
     }
 
-    public void generateResults() throws NullPointerException {
-        File results = new File("results/report.txt");
+    public void generateResults() throws FileNotFoundException {
         try {
+            File results = new File("results/report.txt");
             boolean createdNewFile = results.createNewFile();
-            if (createdNewFile) {
-                System.out.println("File created @ " + results.getCanonicalPath());
-                PrintWriter output = new PrintWriter(results);
 
-                int i = 0;
-                for (String studentAnswer:studentAnswers) {
-                    String yourAnswer = "Your answer was: " + studentAnswer;
-                    String cachedAnswer = "The correct answer is: " + cachedAnswers[i];
-                    boolean answerIsCorrect = studentAnswer.equals(cachedAnswers[i]);
-                    output.println(yourAnswer + " | " + cachedAnswer + " || " + (answerIsCorrect ? "Correct!" : "Incorrect..."));
-                    i++;
-                }
-                output.println("\n" + "**************************************************************" + "\n");
-                output.println("Total score: " + (this.score)+ "/20" + "\n");
-
-                String finalResult = "Congratulations! You passed with flying colors!";
-                if (this.score < 15) {
-                    finalResult = "Sorry, you didn't pass. Try again soon!";
-                }
-                output.println(finalResult);
-                output.close();
-            } else {
-                throw new Exception("File already exists.");
+            if (!createdNewFile) {
+                throw new FileAlreadyExistsException("File already exists.");
             }
-        } catch (Exception err) {
-            // TODO Add logic to create new file w/ name like results_1, results_2, etc. and rerun recursively?
-            System.out.println("Exception: " + err.getMessage());
+
+            PrintWriter output = new PrintWriter(results);
+            int i = 0;
+            for (String studentAnswer:studentAnswers) {
+                String yourAnswer = "Your answer was: " + studentAnswer;
+                String cachedAnswer = "The correct answer is: " + cachedAnswers[i];
+                boolean answerIsCorrect = studentAnswer.equals(cachedAnswers[i]);
+                output.println(yourAnswer + " | " + cachedAnswer + " || " + (answerIsCorrect ? "Correct!" : "Incorrect..."));
+                i++;
+            }
+            output.println("\n" + "**************************************************************" + "\n");
+
+            output.println("Total score: " + (this.score)+ "/20" + "\n");
+            String finalResult = "Congratulations! You passed with flying colors!";
+            if (this.score < 15) {
+                finalResult = "Sorry, you didn't pass. Try again soon!";
+            }
+            output.println(finalResult);
+
+            output.close();
+        } catch (FileAlreadyExistsException err) {
+            System.out.println("Error: File already exists! Please move, rename, or delete existing file.");
+            System.out.println(err.getMessage());
             err.printStackTrace();
+            System.exit(2);
+        } catch (IOException err) {
+            System.out.println("Something went wrong when finding or creating the file.");
+            System.out.println(err.getMessage());
+            err.printStackTrace();
+            System.exit(2);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
-import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
+        FileReader reader = new FileReader();
         try {
-            FileReader reader = new FileReader();
-            File questions = new File("assets/questions.txt");
-            reader.readFile(questions, reader::askQuestion);
-            File answers = new File("assets/DMVanswers.txt");
-            reader.readFile(answers, reader::cacheAndCheckAnswers);
+            System.out.println("Please enter only 'A', 'B', 'C', or 'D' to answer each question. Case does not matter. Enter 'quit' to exit application.");
+            reader.readFile("assets/questions.txt", reader::askQuestion);
+            reader.readFile("assets/DMVanswers.txt", reader::cacheAndCheckAnswers);
             reader.generateResults();
-        } catch (Exception err) {
-            System.out.println("Exception: " + err.getMessage());
+        } catch (FileNotFoundException err) {
+            System.out.println("Error: File not found! Please confirm that you entered the correct file URI and try again.");
+            System.out.println(err.getMessage());
             err.printStackTrace();
+            System.exit(2);
+        }  catch (ArrayIndexOutOfBoundsException err) {
+            System.out.println("Error: You may have made a mistake when entering the total number of questions and/or answers. Please confirm you input the correct numbers and try again.");
+            System.out.println("You entered: " + err.getMessage());
+            err.printStackTrace();
+            System.exit(2);
+        } catch (Exception err) {
+            System.out.println("Error: Something went wrong.");
+            System.out.println(err.getMessage());
+            err.printStackTrace();
+            System.exit(2);
         }
     }
 }
